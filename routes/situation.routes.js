de510c5bcef92e2c72df6ca52990fcc78bcfeba8
LAymeric <aymeric.larvet@gmail.com>
2020-05-17 19:23:14 +0200

    :sparkles: creating post for situation + todos

--- routes/situation.routes.js
+++ routes/situation.routes.js
@@ -2,6 +2,7 @@ const UserController = require('../controllers/user.controller');
 const CharacteristicController = require('../controllers/characteristic.controller');
 const AllergenController = require('../controllers/alleregen.controller');
 const SituationBean = require('../beans/situation.bean');
+const bodyParser = require('body-parser');
 
 module.exports = function(app) {
     app.get('/situation/:token', async (req, res) => {
@@ -15,10 +16,22 @@ module.exports = function(app) {
         userAllergens.forEach(userAllergen => {
             situationBean.allergens.push(userAllergen.id)
         });
+        // todo check with real data
+
         if(userId && userCharacteristics && userAllergens){
             res.status(200).json({
                 ...situationBean
             });
+        } else {
+            res.status(500).end();
+        }
+    });
+    app.post('/situation/:token', bodyParser.json(), async (req, res) => {
+        const userId = await UserController.get_user_id_by_token(req.params.token)
+        // todo update data
+
+        if(userId){
+            res.status(200);
         } else {
             res.status(500).end();
         }
