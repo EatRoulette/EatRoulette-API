7341170f325e176bce58393731e5ef16c1fda24b
Pothier Lucas <barder60>
2020-06-02 13:17:30 +0200

    :sparkles: new systeme Crawler

--- controllers/crawler.controller.js
+++ controllers/crawler.controller.js
@@ -14,7 +14,33 @@ class FriendsListUserController extends CoreController {
         return super.render(list, { ...options,populates});
     }
 
-    static crawlerRender
+    static async renderDishData(req,res,next) {
+        const id = req.params.DishName;
+        /*Promise.resolve()
+            .then(() => FriendsListUserController.friendsListUserNotExist(req,res,next,id))
+            .then(() => {
+                FriendsListUserModel
+                    .findById(id).populate("users")
+                    .select('name users _id')
+                    .exec()
+                    .then(doc => {
+                        if(doc){
+                            res.status(200).json({
+                                friendsListUser: doc,
+                                request: {
+                                    type: 'GET',
+                                    url: `http://localhost:3000/friendsListUsers`,
+                                }
+                            });
+                        }
+                    }).catch(err => {
+                    res.status(400).json({
+                        message: "Bad request",
+                        err,
+                    });
+                });
+            });*/
+    };
 }
 
 FriendsListUserController.prototype.modelName = 'FriendsListUser';
