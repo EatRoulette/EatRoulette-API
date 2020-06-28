687adbc2119bd55c00b13f217f5aa982bed9b594
Gwen <gwendou95@hotmail.fr>
2020-06-28 19:07:11 +0200

    :sparkles: Add ticket admin management

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -11,4 +11,35 @@ module.exports = function(app) {
     app.post('/ticket/create/', bodyParser.json(), TicketController.create_ticket);
     app.delete('/ticket/:id/comments/:comment', TicketController.delete_comment_of_ticket);
     app.post('/ticket/:id/comments/:idAuthor', bodyParser.json(), TicketController.add_comment_to_ticket);
+
+    /**
+     * Ticket desk management
+     */
+
+    app.get('/ticket/desk/open', async (req, res) => {
+        const tickets = await TicketController.getOpenTickets();
+
+        if (tickets.length > 0){
+            res.status(200).json(tickets);
+        } else {
+            res.status(204).end()
+        }
+
+        res.status(500).end();
+    });
+
+    app.put('/ticket/desk/:id/:status', async (req, res) => {
+        const ret = await TicketController.updateTicketStatus(req.params.id, req.params.status);
+
+        console.log(ret)
+
+       if (ret === -1 ){
+           res.status(400).end()
+       } else {
+           res.status(200).end();
+       }
+
+        res.status(500).end();
+    });
+
 };
